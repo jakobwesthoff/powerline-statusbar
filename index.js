7531d8d72fea0c975d491ace0d02774e422a537d
Jakob Westhoff <jakob@westhoffswelt.de>
2015-04-17 12:43:16 +0200

    Export new Segments

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import PowerlineStatus from "./Library/PowerlineStatus";
-import StaticSegment from "./Library/StaticSegment";
 
+import StaticSegment from "./Library/Segment/Static";
+import StartTimeSegment from "./Library/Segment/StartTime";
+import CounterSegment from "./Library/Segment/Counter";
+import PerSecondSegment from "./Library/Segment/PerSecond";
+import RateSegment from "./Library/Segment/Rate";
 
-export {PowerlineStatus, StaticSegment};
+export {
+    PowerlineStatus,
+    StaticSegment,
+    StartTimeSegment,
+    CounterSegment,
+    PerSecondSegment,
+    RateSegment
+};
 
