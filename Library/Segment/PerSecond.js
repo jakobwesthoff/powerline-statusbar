2e84b7c3701e15789f83d3060ce5c7017a0a653a
Jakob Westhoff <jakob@westhoffswelt.de>
2015-04-17 12:42:44 +0200

    Implemented PerSecond Segment

---
+++ Library/Segment/PerSecond.js
@@ -0,0 +1,33 @@
+import BaseSegment from "./Base";
+import _ from "lodash";
+import moment from "moment";
+
+var defaultOptions = {
+    prefix: "",
+    postfix: ""
+};
+
+export default class PerSecondSegment extends BaseSegment {
+    constructor(startValue, options) {
+        options  = _.merge({}, defaultOptions, options);
+        super(options);
+
+        this.startValue = startValue;
+        this.resetCounter();
+    }
+
+    resetCounter(startValue = this.startValue) {
+        this.counterValue = startValue;
+        this.startTime = new Date();
+    }
+
+    step(size = 1) {
+        this.counterValue += size;
+    }
+
+    provideContent() {
+        var timeDifferenceInSeconds = moment(this.startTime).diff(new Date(), "seconds");
+        var rate = Math.round((this.counterValue/timeDifferenceInSeconds)*10)/10;
+        return `${this.options.prefix}${rate}${this.options.postfix}`;
+    }
+}
